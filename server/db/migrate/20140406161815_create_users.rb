95125f85c5bcfc3c6566fcdef5d81d896f417206
4urbanoff <4urbanoff@gmail.com>
2015-09-25 16:13:35 +0300

    fix

--- server/db/migrate/20140406161815_create_users.rb
+++ server/db/migrate/20140406161815_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string  :token,  limit: 32
-      t.string  :name,   limit: 100
+      t.string  :name,   limit: 30
 
       t.timestamps null: true
 
